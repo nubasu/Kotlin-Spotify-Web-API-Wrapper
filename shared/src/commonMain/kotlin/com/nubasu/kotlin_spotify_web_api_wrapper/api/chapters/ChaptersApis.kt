bd12452476c37ab3763d2f6cda85e0306ef0cb08
nubasu <nubasu>
2026-01-18 19:24:19 +0900

    feat: implement chapter apis

---
+++ shared/src/commonMain/kotlin/com/nubasu/kotlin_spotify_web_api_wrapper/api/chapters/ChaptersApis.kt
@@ -0,0 +1,67 @@
+package com.nubasu.kotlin_spotify_web_api_wrapper.api.chapters
+
+import com.nubasu.kotlin_spotify_web_api_wrapper.response.chapters.Chapter
+import com.nubasu.kotlin_spotify_web_api_wrapper.response.chapters.Chapters
+import com.nubasu.kotlin_spotify_web_api_wrapper.utils.CountryCode
+import com.nubasu.kotlin_spotify_web_api_wrapper.utils.TokenHolder
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.accept
+import io.ktor.client.request.bearerAuth
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
+import io.ktor.http.appendPathSegments
+import io.ktor.http.isSuccess
+import io.ktor.http.takeFrom
+import io.ktor.serialization.kotlinx.json.json
+
+class ChaptersApis {
+    private val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
+
+    suspend fun getAChapter(
+        id: String,
+        market: CountryCode? = null,
+    ) : Chapter {
+        val ENDPOINT = "https://api.spotify.com/v1/chapters/"
+        val response = client.get {
+            url {
+                takeFrom(ENDPOINT)
+                appendPathSegments(id)
+                market?.let { parameters.append("market", it.code) }
+            }
+            bearerAuth(TokenHolder.token)
+            accept(ContentType.Application.Json)
+        }
+        if (!response.status.isSuccess()) {
+            throw RuntimeException("Spotify error ${response.status}: ${response.bodyAsText()}")
+        }
+        return response.body()
+    }
+
+    suspend fun getSeveralChapters(
+        ids: List<String>,
+        market: CountryCode? = null,
+    ) : Chapters {
+        val ENDPOINT = "https://api.spotify.com/v1/albums"
+        val response = client.get {
+            url {
+                takeFrom(ENDPOINT)
+                parameters.append("ids", ids.joinToString(","))
+                market?.let { parameters.append("market", market.code) }
+            }
+            bearerAuth(TokenHolder.token)
+            accept(ContentType.Application.Json)
+        }
+        if (!response.status.isSuccess()) {
+            throw RuntimeException("Spotify error ${response.status}: ${response.bodyAsText()}")
+        }
+        return response.body()
+    }
+}
