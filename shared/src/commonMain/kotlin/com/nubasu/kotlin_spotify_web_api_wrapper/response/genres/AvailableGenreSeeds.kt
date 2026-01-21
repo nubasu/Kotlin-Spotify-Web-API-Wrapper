c65d2fe1c46f1bed11a3bfa76e5b035c56dc8565
nubasu <nubasu>
2026-01-21 20:46:05 +0900

    feat: add @serializable and some adjustment for response

--- shared/src/commonMain/kotlin/com/nubasu/kotlin_spotify_web_api_wrapper/response/genres/AvailableGenreSeeds.kt
+++ shared/src/commonMain/kotlin/com/nubasu/kotlin_spotify_web_api_wrapper/response/genres/AvailableGenreSeeds.kt
@@ -1,5 +1,8 @@
 package com.nubasu.kotlin_spotify_web_api_wrapper.response.genres
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class AvailableGenreSeeds(
     val genres: List<String>,
 )
