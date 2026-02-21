d44d6dee436aa0bfc32f3608bcea417c2cfe7bf2
nubasu <nubasu>
2026-02-21 17:24:31 +0900

    feat: add DebugTokenProvider

--- composeApp/src/jvmMain/kotlin/com/nubasu/kotlin_spotify_web_api_wrapper/main.kt
+++ composeApp/src/jvmMain/kotlin/com/nubasu/kotlin_spotify_web_api_wrapper/main.kt
@@ -2,12 +2,15 @@ package com.nubasu.kotlin_spotify_web_api_wrapper
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.nubasu.kotlin_spotify_web_api_wrapper.utils.TokenHolder
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "kotlin_spotify_web_api_wrapper",
     ) {
+        installDebugTokenFromLocalProperties()
+        TokenHolder.getTokenFromProvider()
         App()
     }
 }
